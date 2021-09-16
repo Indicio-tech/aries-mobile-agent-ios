edd1f91220def05e1b83c91dd93e50fc234dcb19
David Clawson <david.clawson@gmail.com>
2021-09-16 15:07:12 -0600

    Updated Readme.md to reflect new Framework method, as well as included Xcode files
    Signed-off-by: David Clawson <david.clawson@gmail.com>

---
+++ Aries.framework/Headers/Aries.h
@@ -0,0 +1,18 @@
+//
+//  Aries.h
+//  Aries
+//
+//  Created by David Clawson on 9/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Aries.
+FOUNDATION_EXPORT double AriesVersionNumber;
+
+//! Project version string for Aries.
+FOUNDATION_EXPORT const unsigned char AriesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aries/PublicHeader.h>
+
+
