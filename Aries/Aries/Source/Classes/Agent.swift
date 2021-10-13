a1560d4c4c51204fa414821f00ef43531c3c079e
Patrick Kenyon <treek.kenyon@gmail.com>
2021-10-13 14:21:36 -0600

    Added initial connections module
    Signed-off-by: Patrick Kenyon <treek.kenyon@gmail.com>

--- Aries/Aries/Source/Classes/Agent.swift
+++ Aries/Aries/Source/Classes/Agent.swift
@@ -8,19 +8,29 @@
 import Foundation
 import Indy
 
-public class Agent {
-    private var ariesWallet: AriesWallet
+public class AriesAgent {
+    private var ariesWallet: AriesWallet? = nil
+    private var messageSender: MessageSender? = nil
+    private var messageReceiver: MessageReceiver? = nil
+    private var storage: Storage? = nil
+    public var connections: AriesConnections? = nil
     
 //  For this implementation, we're using a default wallet with the "default" identifier and "password" as the key.
 //  We'll set up the ability to set/change passwords in later versions.
     
-    public init(){
+    public init(completion: @escaping (_ result: Result<Void, Error>)->Void){
         ariesWallet = AriesWallet(){ result in
             switch(result){
             case(.success()):
                 print("Wallet opened")
+                self.messageReceiver = MessageReceiver(wallet: self.ariesWallet!)
+                self.messageSender = MessageSender(ariesWallet: self.ariesWallet!, messageReceiver: self.messageReceiver!)
+                self.storage = Storage(ariesWallet: self.ariesWallet!)
+                self.connections = AriesConnections(ariesWallet: self.ariesWallet!, messageSender: self.messageSender!, storage: self.storage!)
+                completion(.success(()))
             case(.failure(let e)):
                 print(e.localizedDescription)
+                completion(.failure(e))
             }
         }
     }
